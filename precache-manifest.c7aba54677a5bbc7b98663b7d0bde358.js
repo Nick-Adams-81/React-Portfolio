ece337807c155198b51b9699c734d80fdfb8259d
nick adams <thebigdeal916@icloud.com>
2020-05-15 11:56:48 -0700

    Updates

---
+++ precache-manifest.c7aba54677a5bbc7b98663b7d0bde358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82097ee557c27c72ff6a61bab7337d7a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "10cd84f75fd263661613",
+    "url": "/React-Portfolio/static/css/main.b10c7919.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/React-Portfolio/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cd84f75fd263661613",
+    "url": "/React-Portfolio/static/js/main.6d1f6b0d.chunk.js"
+  },
+  {
+    "revision": "b71c51bcb25dd14b448b",
+    "url": "/React-Portfolio/static/js/runtime-main.b80a39b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
