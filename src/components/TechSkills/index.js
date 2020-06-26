a76a2fc39703f74e8ee98f37cbf4e920e4733fb5
nick adams <thebigdeal916@icloud.com>
2020-06-26 09:44:57 -0700

    tech skills content added

--- src/components/TechSkills/index.js
+++ src/components/TechSkills/index.js
@@ -4,7 +4,22 @@ import './style.css';
 
 function TechSkills () {
     return (
-        <div>
+        <div class="container">
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>javaScript</li>
+                <li>jQuerry</li>
+                <li>node.js</li>
+                <li>express</li>
+                <li>handlebars</li>
+                <li>mySql</li>
+                <li>sequelize</li>
+                <li>mongoose</li>
+                <li>mongoDb</li>
+                <li>React</li>
+            </ul>
 
         </div>
     )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,8 @@ import Jumbotron from "../Jumbotron";
 import Content from "../Content";
 import Images from "../Images";
 import Link from "../Link";
-import Email from "../Email"
+import Email from "../Email";
+import TechSkills from "../TechSkills"
 import "./style.css"
 
 
@@ -34,6 +35,7 @@ function Home() {
         <div class="row">
           <div class="col-sm-12">
             <h2>Technical Skills</h2>
+            <TechSkills />
           </div>
         </div>
       </div>
