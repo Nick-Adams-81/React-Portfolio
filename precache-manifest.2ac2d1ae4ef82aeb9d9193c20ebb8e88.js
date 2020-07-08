42a52e3fbe3643224c1c1f343f511108f6cba5eb
nick adams <thebigdeal916@icloud.com>
2020-07-08 10:33:53 -0700

    Updates

---
+++ precache-manifest.2ac2d1ae4ef82aeb9d9193c20ebb8e88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a61ac647064b40d9a9145e764416f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a23d39cfa429fa6034",
+    "url": "/static/css/main.7b13154c.chunk.css"
+  },
+  {
+    "revision": "c148e9156cc3ac0a75cf",
+    "url": "/static/js/2.5fefd1ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5fefd1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a23d39cfa429fa6034",
+    "url": "/static/js/main.3d67353e.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "bc7aafe074b9feb9bdde2bb100927e6f",
+    "url": "/static/media/linkedin.bc7aafe0.png"
+  },
+  {
+    "revision": "12f61a86a91640c78c291559f2009341",
+    "url": "/static/media/styx.12f61a86.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.e399a36abee2c3c3565b6fb2cac70ca9.js"
+  "/precache-manifest.2ac2d1ae4ef82aeb9d9193c20ebb8e88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
